ae4636fcb573e7f45b221e925d2941e09f88c7ce
shrutanten <shrutanten.work@gmail.com>
2023-08-24 15:04:40 +0530

    Add all functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CounterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CounterComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.scss']
+  styleUrls: ['./counter.component.scss'],
 })
 export class CounterComponent {
+  counterValue = 0;
+  inputValue = 0;
 
+  constructor() {}
+
+  incrementCounter() {
+    this.counterValue += 1;
+  }
+
+  decrementCounter() {
+    if (this.counterValue <= this.inputValue) return;
+
+    this.counterValue -= 1;
+  }
 }
